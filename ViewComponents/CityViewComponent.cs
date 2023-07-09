a8e395e96ecd15bfb8f73ca45effe509f4188006
Mima80 <mimi451276@gmail.com>
2023-07-09 23:43:41 +0300

    Changed PartialView to ViewComponent

---
+++ ViewComponents/CityViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Drawing;
+using WeatherApp.Models;
+
+namespace WeatherApp.ViewComponents
+{
+    public class CityViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(CityWeather cityWeather)
+        {
+            var color = "";
+            switch (cityWeather.TemperatureFahrenheit)
+            {
+                case < 44:
+                    color = "blue-back";
+                    break;
+                case >= 44 and < 74:
+                    color = "yellow-back";
+                    break;
+                case > 74:
+                    color = "orange-back";
+                    break;
+            }
+            ViewBag.Color = color;
+            return View(cityWeather); //invokes view of the view component at Views/Shared/Components/Grid/Sample.cshtml
+        }
+    }
+}
